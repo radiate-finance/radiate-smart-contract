5849714a5248f932c817edcb3c70f05c1b2ab8d4
shwetal <sonishwetal704@gmail.com>
2021-08-23 12:07:01 +0530

    local testing successfully

--- smart_contracts/contract.py
+++ smart_contracts/contract.py
@@ -1,5 +1,4 @@
 import smartpy as sp
-math = sp.io.import_script_from_url("file:smart_contracts/math.py").Calculations()
 
 TokenType = sp.TVariant(
     tez = sp.TUnit,
@@ -35,41 +34,43 @@ class Radiate(sp.Contract):
     def checkStopTime(self, startTime, stopTime):
         sp.verify(stopTime > startTime, message = "Stop time must be greater than start time")
 
-    def getDeposit(self, startTime, stopTime, ratePerSecond):
-        duration = math.subtract(stopTime, startTime)
-        return duration * ratePerSecond
+    @sp.sub_entry_point
+    def getDeposit(self, params):
+        sp.verify(params.stopTime >= params.startTime)
+        duration = sp.as_nat(params.stopTime - params.startTime)
+        sp.result(duration * params.ratePerSecond)
 
     def checkValidReceiver(self, receiver, sender):
         sp.verify(receiver != sender, message = "Invalid Receiver")
-
+    
+    @sp.sub_entry_point
     def timeDifference(self, streamId):
         stream = self.data.streams[streamId]
-        _startTime = sp.timestamp(stream.startTime)
-        _stopTime = sp.timestamp(stream.stopTime)
 
-        sp.if sp.now < _startTime:
-            return 0
-        sp.if sp.now < _stopTime:
-            return sp.now - _startTime
-        
-        return _stopTime - _startTime
+        sp.if (sp.now < stream.startTime):
+            sp.result(sp.as_nat(0))
+        sp.else:
+            sp.if (stream.stopTime > sp.now):
+                sp.result(sp.as_nat(sp.now - stream.startTime))
+            sp.else:
+                sp.result(sp.as_nat(stream.stopTime - stream.startTime))
 
-    def balanceOfReceiver(self, streamId):  
-        stream = self.data.streams[streamId]
 
-        receiverBalance = self.timeDifference(streamId) * stream.ratePerSecond
+    @sp.sub_entry_point
+    def balanceOfReceiver(self, params):  
+        stream = self.data.streams[params.streamId]
+
+        receiverBalance = params.timeDifference * stream.ratePerSecond
 
         sp.if stream.remainingBalance < stream.deposit:
-            withdrawalAmount = stream.deposit - stream.remainingBalance
-            receiverBalance = receiverBalance - withdrawalAmount
+            withdrawalAmount = sp.as_nat(stream.deposit - stream.remainingBalance)
+            receiverBalance = sp.as_nat(receiverBalance - withdrawalAmount)
 
-        return receiverBalance
+        sp.result(receiverBalance)
 
-    def balanceOfSender(self, streamId):
-        return self.data.streams[streamId].remainingBalance - self.balanceOfReceiver(streamId)
 
     def checkRequester(self, streamId):
-        sp.verify((sp.sender == self.data.streams[streamId].receiver) | sp.sender == self.data.streams[streamId].sender, message = "Requester should be either sender or receiver of stream")
+        sp.verify((sp.sender == self.data.streams[streamId].receiver), message = "Requester should be receiver of stream")
         pass
 
     @sp.entry_point
@@ -86,7 +87,13 @@ class Radiate(sp.Contract):
         self.checkStopTime(params.startTime, params.stopTime)
         self.checkValidReceiver(params.receiver, sp.sender)
 
-        deposit = sp.local("deposit", self.getDeposit(params.startTime, params.stopTime, params.ratePerSecond))
+        deposit = sp.local("deposit", self.getDeposit(sp.record(
+                    startTime = params.startTime, 
+                    stopTime = params.stopTime, 
+                    ratePerSecond =  params.ratePerSecond
+                )
+            )
+        )
 
         # transfer tokens
         with params.token.match_cases() as arg:
@@ -101,16 +108,48 @@ class Radiate(sp.Contract):
                 ).layout(("from_ as from", ("to_ as to", "value")))
 
                 c = sp.contract(data_type, FA12_token, "transfer").open_some()
-                data_to_be_sent = sp.record(from_ = sp.sender, to_ = sp.self_address, value = deposit.value)
+
+                data_to_be_sent = sp.record(
+                    from_ = sp.sender,
+                    to_ = sp.self_address,
+                    value = deposit.value
+                )
+
                 sp.transfer(data_to_be_sent, sp.mutez(0), c)
 
             with arg.match("FA2") as FA2_token:
-                data_type = sp.TRecord(
-                    from_ = sp.TAddress,
-                    to_ = sp.TAddress,
-                    amount = sp.TNat
-                ).layout(("from_ as from", ("to_ as to", "value")))
+                data_type = sp.TList(
+                    sp.TRecord(
+                        from_ = sp.TAddress, 
+                        txs = sp.TList(
+                            sp.TRecord(
+                                amount = sp.TNat,
+                                to_ = sp.TAddress,
+                                token_id = sp.TNat
+                            ).layout(("to_", ("token_id", "amount")))
+                        )
+                    ).layout(("from_", "txs"))
+                )
 
+                c = sp.contract(data_type, FA2_token.tokenAddress, "transfer").open_some()
+
+                data_to_be_sent = sp.list(
+                    [
+                        sp.record(
+                            from_ = sp.sender, 
+                            txs = sp.list(
+                                [
+                                    sp.record(
+                                        amount = deposit.value,
+                                        to_ = sp.self_address, 
+                                        token_id = FA2_token.tokenId
+                                    )
+                                ]
+                            )
+                        )
+                    ]
+                )
+                sp.transfer(data_to_be_sent, sp.mutez(0), c)
 
 
         self.data.streams[self.data.nextStreamId] = sp.record(
@@ -126,49 +165,188 @@ class Radiate(sp.Contract):
 
         self.data.nextStreamId = self.data.nextStreamId + 1
 
-    # @sp.entry_point
-    # def withdraw(self, params):
+    @sp.entry_point
+    def withdraw(self, params):
 
-    #     sp.set_type(params, sp.TRecord(
-    #         streamId = sp.TNat,
-    #         amount = sp.TNat
-    #     ))
+        sp.set_type(params, sp.TRecord(
+            streamId = sp.TNat,
+            amount = sp.TNat
+        ))
 
-    #     self.checkRequester(params.streamId)
-    #     sp.verify(params.amount > 0, message = "Amount should be greater than zero")   
+        self.checkRequester(params.streamId)
+        sp.verify(params.amount > 0, message = "Amount should be greater than zero")   
 
-    #     stream = self.data.streams[params.streamId]
+        stream = self.data.streams[params.streamId]
+        timeDiff = self.timeDifference(params.streamId)
 
-    #     # balance should be greater than requested amount
-    #     balance = self.balanceOfReceiver(params.streamId)
-    #     sp.verify(balance >= params.amount, message = "Not enough amount in your balance")
+        # balance should be greater than requested amount
+        balance = self.balanceOfReceiver(sp.record(streamId = params.streamId, timeDifference = timeDiff))
+        sp.verify(balance >= params.amount, message = "Not enough amount in your balance")
         
-    #     self.data.stream.remainingBalance = self.data.stream.remainingBalance - params.amount
+        stream.remainingBalance = sp.as_nat(stream.remainingBalance - params.amount)
 
-    #     # if remaining balance becomes 0..delete stream
-    #     sp.if stream.remainingBalance == 0:
-    #         del stream
+        # token transfer
+        with self.data.streams[params.streamId].token.match_cases() as arg:
+            with arg.match("tez") as unit:
+                sp.send(sp.sender, sp.utils.nat_to_mutez(params.amount), message = "Withdrawal from stream")
 
-    #     # token transfer
-        
-    # @sp.entry_point
-    # def cancelStream(self, params):
+            with arg.match("FA12") as FA12_token:
+                data_type = sp.TRecord(
+                    from_ = sp.TAddress,
+                    to_ = sp.TAddress,
+                    value = sp.TNat
+                ).layout(("from_ as from", ("to_ as to", "value")))
 
-    #     sp.set_type(params, sp.TRecord(
-    #         streamId = sp.TNat,
-    #     ))
+                c = sp.contract(data_type, FA12_token, "transfer").open_some()
 
-    #     self.checkRequester(params.streamId)
+                data_to_be_sent = sp.record(
+                    from_ = sp.self_address, 
+                    to_ = sp.sender, 
+                    value = params.amount
+                )
 
-    #     # senderBalance = self.balanceOfSender(params.streamId)
-    #     # receiverBalance = self.balanceOfReceiver(params.streamId)
+                sp.transfer(data_to_be_sent, sp.mutez(0), c)
 
-    #     stream = self.data.streams[params.streamId]
+            with arg.match("FA2") as FA2_token:
+                data_type = sp.TList(
+                    sp.TRecord(
+                        from_ = sp.TAddress, 
+                        txs = sp.TList(
+                            sp.TRecord(
+                                amount = sp.TNat, 
+                                to_ = sp.TAddress,
+                                token_id = sp.TNat
+                            ).layout(("to_", ("token_id", "amount")))
+                        )
+                    ).layout(("from_", "txs"))
+                )
 
-    #     del stream
+                c = sp.contract(data_type, FA2_token.tokenAddress, "transfer").open_some()
 
-    #     # Cancels the stream and transfers the tokens back on a pro rata basis.
-    #     # token transfer
+                data_to_be_sent = sp.list(
+                    [
+                        sp.record(
+                            from_ = sp.self_address, 
+                            txs = sp.list(
+                                [
+                                    sp.record(
+                                        amount = params.amount, 
+                                        to_ = sp.sender, 
+                                        token_id = FA2_token.tokenId
+                                    )
+                                ]
+                            )
+                        )
+                    ]
+                )
+                sp.transfer(data_to_be_sent, sp.mutez(0), c)
+
+        # if remaining balance becomes 0 then delete stream
+        sp.if stream.remainingBalance == 0:
+            del stream
 
 
+    @sp.entry_point
+    def cancelStream(self, params):
 
+        sp.set_type(params, sp.TRecord(
+            streamId = sp.TNat,
+        ))
+    
+        sp.verify(
+            (sp.sender == self.data.streams[params.streamId].sender) | (sp.sender == self.data.streams[params.streamId].receiver)
+        )
+
+        timeDiff = self.timeDifference(params.streamId)
+        # sp.operations().push(timeDiff)
+
+        receiverBalance = self.balanceOfReceiver(sp.record(
+                streamId = params.streamId, 
+                timeDifference = timeDiff
+            )
+        )
+        senderBalance = sp.as_nat(self.data.streams[params.streamId].remainingBalance - receiverBalance)
+
+        stream = self.data.streams[params.streamId]
+
+        # token transfer
+        with self.data.streams[params.streamId].token.match_cases() as arg:
+            with arg.match("tez") as unit:
+                sp.send(self.data.streams[params.streamId].sender, sp.utils.nat_to_mutez(senderBalance), message = "Stream canceled")
+                sp.send(self.data.streams[params.streamId].receiver, sp.utils.nat_to_mutez(receiverBalance), message = "Stream canceled")
+
+            with arg.match("FA12") as FA12_token:
+                data_type = sp.TRecord(
+                    from_ = sp.TAddress,
+                    to_ = sp.TAddress,
+                    value = sp.TNat
+                ).layout(("from_ as from", ("to_ as to", "value")))
+
+                c = sp.contract(data_type, FA12_token, "transfer").open_some()
+
+                data_to_be_sent = sp.record(          # sending to sender
+                    from_ = sp.self_address,
+                    to_ = self.data.streams[params.streamId].sender,
+                    value = senderBalance
+                )
+                sp.transfer(data_to_be_sent, sp.mutez(0), c)
+
+                data_to_be_sent_receiver = sp.record(       # sending to receiver
+                    from_ = sp.self_address,
+                    to_ = self.data.streams[params.streamId].receiver,
+                    value = receiverBalance
+                )
+                sp.transfer(data_to_be_sent_receiver, sp.mutez(0), c)
+
+            with arg.match("FA2") as FA2_token:
+                data_type = sp.TList(
+                    sp.TRecord(
+                        from_ = sp.TAddress, 
+                        txs = sp.TList(
+                            sp.TRecord(amount = sp.TNat,
+                                to_ = sp.TAddress,
+                                token_id = sp.TNat
+                            ).layout(("to_", ("token_id", "amount")))
+                        )
+                    ).layout(("from_", "txs"))
+                )
+
+                c = sp.contract(data_type, FA2_token.tokenAddress, "transfer").open_some()
+
+                data_to_be_sent = sp.list(   # for sender
+                    [
+                        sp.record(
+                            from_ = sp.self_address, 
+                            txs = sp.list(
+                                [
+                                    sp.record(
+                                        amount = senderBalance,
+                                        to_ = self.data.streams[params.streamId].sender,
+                                        token_id = FA2_token.tokenId
+                                    )
+                                ]
+                            )
+                        )
+                    ]
+                )
+                sp.transfer(data_to_be_sent, sp.mutez(0), c)
+
+                data_to_be_sent_receiver = sp.list(      # for receiver
+                    [       
+                        sp.record(
+                            from_ = sp.self_address, 
+                            txs = sp.list(
+                                [
+                                    sp.record(
+                                        amount = receiverBalance,
+                                        to_ = self.data.streams[params.streamId].receiver,
+                                        token_id = FA2_token.tokenId
+                                    )
+                                ]
+                            )
+                        )
+                    ]
+                )
+                sp.transfer(data_to_be_sent_receiver, sp.mutez(0), c)
+
+        del stream
--- tests.py
+++ tests.py
@@ -1,21 +1,15 @@
 import smartpy as sp
 
 Radiate = sp.io.import_script_from_url("file:smart_contracts/contract.py").Radiate
-FA12 = sp.io.import_script_from_url("file:FA12.py").FA12
-# FA12_config = sp.io.import_script_from_url("file:FA12.py").FA12_config
 
-class FA12_config:
-    def __init__(
-        self,
-        support_upgradable_metadata         = False,
-        use_token_metadata_offchain_view    = True,
-    ):
-        self.support_upgradable_metadata = support_upgradable_metadata
-        # Whether the contract metadata can be upgradable or not.
-        # When True a new entrypoint `change_metadata` will be added.
+FA12_module = sp.io.import_script_from_url("https://smartpy.io/dev/templates/FA1.2.py")
+FA12 = FA12_module.FA12
+FA12_config = FA12_module.FA12_config
 
-        self.use_token_metadata_offchain_view = use_token_metadata_offchain_view
-        # Include offchain view for accessing the token metadata (requires TZIP-016 contract metadata)
+FA2_module = sp.io.import_script_from_url("https://smartpy.io/dev/templates/FA2.py")
+FA2 = FA2_module.FA2
+FA2_config = FA2_module.FA2_config
+
 
 @sp.add_test(name = "Radiate", is_default = True)
 def test():
@@ -28,8 +22,8 @@ def test():
     admin = sp.test_account("admin").address
     alice = sp.test_account("alice").address
     bob = sp.test_account("bob").address
-    shwetal = sp.test_account("shwetal").address
-    anshit = sp.test_account("anshit").address
+    user1 = sp.test_account("user1")
+    user2 = sp.test_account("user2")
 
     scenario.h2("Initialise the contract")
     c1 = Radiate()
@@ -58,54 +52,114 @@ def test():
     scenario.h2("Creating first stream")
     c1.createStream(                           # todo
         ratePerSecond = sp.nat(10),
-        startTime = sp.timestamp(200),
-        stopTime = sp.timestamp(500),
-        receiver = bob,
+        startTime = sp.timestamp(1729622656),
+        stopTime = sp.timestamp(1829622656),
+        receiver = user2.address,
         token = sp.variant("tez", sp.unit)
-    ).run(sender = alice, amount = sp.mutez(3000))
+    ).run(sender = alice, amount = sp.mutez(1000000000))
 
-    scenario.h2("Creating second stream")
-    c1.createStream(                           # todo
+    scenario.h2("The failing withdraw case")
+    c1.withdraw(
+    streamId = 0,
+    amount = 10
+    ).run(sender = user2.address, now = sp.timestamp(100))
+
+    scenario.h2("Withdrawing from stream of tez")
+    c1.withdraw(
+    streamId = 0,
+    amount = 10
+    ).run(sender = user2.address, now = sp.timestamp(1759622656))
+
+    scenario.h2("To test if withdrawal works after stopTime")
+    c1.withdraw(
+    streamId = 0,
+    amount = 10
+    ).run(sender = user2.address, now = sp.timestamp(1929622656))
+
+    scenario.h2("Withdraw before stopTime")
+    c1.withdraw(
+    streamId = 0,
+    amount = 10
+    ).run(sender = user2.address, now = sp.timestamp(1819622656))
+
+
+    scenario.h2("Creating second stream for FA1.2 token")
+    scenario.h3("Failing case, before minting")
+    c1.createStream(                           
         ratePerSecond = sp.nat(100),
         startTime = sp.timestamp(500),
         stopTime = sp.timestamp(1000),
-        receiver = anshit,
+        receiver = user2.address,
         token = sp.variant("FA12", c2.address)
-    ).run(sender = shwetal, valid = False)
+    ).run(sender = user1.address, valid = False)
 
-    c2.mint(address = shwetal, value = 60000).run(sender = admin)
-    c2.approve(spender = c1.address, value = 50000).run(sender = shwetal)
+    c2.mint(address = user1.address, value = 60000).run(sender = admin)   #minting
+    c2.approve(spender = c1.address, value = 50000).run(sender = user1)   #approving
 
-    c1.createStream(                           # todo
+    scenario.h2("Creating stream for FA1.2 after minting")
+    c1.createStream(                           
         ratePerSecond = sp.nat(100),
         startTime = sp.timestamp(500),
         stopTime = sp.timestamp(1000),
-        receiver = anshit,
+        receiver = user2.address,
         token = sp.variant("FA12", c2.address)
-    ).run(sender = shwetal)
+    ).run(sender = user1, now = sp.timestamp(300))
 
-    # scenario.h2("Withdraw little amount from first stream")
-    # c1.withdraw(
-    #     streamId = 0,
-    #     amount = 10
-    # )
 
-    # scenario.h2("Withdraw left out amount from first stream")
-    # c1.withdraw(
-    #     streamId = 0,
-    #     amount = 10
-    # )
+    ######################################### FA2 begins here #####################################
 
-    # scenario.h2("Withdraw from second stream") # withdraw complete amount in one go
-    # c1.withdraw(
-    #     streamId = 1,
-    #     amount = 100
-    # )
+    scenario.h2("Initialise FA2")
 
-    # scenario.h2("Cancel second stream")
-    # c1.cancelStream(
-    #     streamId = 1
-    # )
+    c3 = FA2(
+        config = FA2_config(non_fungible = False),
+        metadata = sp.utils.metadata_of_url("https://example.com"),
+        admin = admin
+    )
+
+    scenario += c3
+
+    scenario.h2("Create stream for FA2, failing case")
+    c1.createStream(                           
+        ratePerSecond = sp.nat(100),
+        startTime = sp.timestamp(500),
+        stopTime = sp.timestamp(1000),
+        receiver = user2.address,
+        token = sp.variant("FA2", sp.record(tokenAddress = c3.address, tokenId = 0))
+    ).run(sender = user1, valid = False)
+
+    scenario.h2("mint tokens")
+    tok0_md = FA2.make_metadata(
+        name = "The Token Zero",
+        decimals = 18,
+        symbol= "TK0" 
+    )
+
+    c3.mint(
+        address = user1.address,
+        amount = 100000,
+        metadata = tok0_md,
+        token_id = 0
+    ).run(sender = admin)
+
+    c3.update_operators(
+        [
+            sp.variant("add_operator", c3.operator_param.make(
+                owner = user1.address,
+                operator = c1.address,
+                token_id = 0
+            ))
+        ]
+    ).run(sender = admin)
+
+    scenario.h2("create stream for FA2, after minting")
+    c1.createStream(                           
+        ratePerSecond = sp.nat(100),
+        startTime = sp.timestamp(500),
+        stopTime = sp.timestamp(1000),
+        receiver = user2.address,
+        token = sp.variant("FA2", sp.record(tokenAddress = c3.address, tokenId = 0))
+    ).run(sender = user1, now = sp.timestamp(400))
+
 
 
 
